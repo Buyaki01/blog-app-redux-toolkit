9fedb5c960eeb8a20b15235c0cc9e77910e76bbf
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-13 18:42:32 +0300

    Add SinglePostPage component

---
+++ src/features/posts/SinglePostPage.js
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux"
+import { selectPostById } from "./postsSlice"
+import PostAuthor from "./PostAuthor"
+import TimeAgo from "./TimeAgo"
+import ReactionButtons from "./ReactionButtons"
+
+const SinglePostPage = () => {
+  // Retrieve postId
+
+  const post = useSelector((state) => selectPostById(state, postId))
+  if (!post) {
+    return (
+      <section>
+        <h2>Post not found!</h2>
+      </section>
+    )
+  }
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <p className="postCredit">
+        <PostAuthor />
+        <TimeAgo />
+      </p>
+      <ReactionButtons post={post} />
+    </article>
+  )
+}
+
+export default SinglePostPage
