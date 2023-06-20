21995a8ebf397956c75cfe7b15dfcf9e9a487e8c
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-20 07:22:26 +0300

    Add authSlice

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,5 +4,6 @@ export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
   tagTypes: ['Post'],
+  
   endpoints: builder => ({})
 })
