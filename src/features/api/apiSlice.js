b08c855fde04ee1ba06e6217dc34f2975182b930
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-20 08:04:59 +0300

    Update apiSlice with baseQuery and baseQueryWithReauth

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,9 +1,46 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { setCredentials, logOut } from '../auth/authSlice'
+
+const baseQuery = fetchBaseQuery({ 
+  baseUrl: 'http://localhost:3500',
+  credentials: 'include',
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`)
+    }
+    return headers
+  }
+})
+
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions)
+
+  if (result?.error?.originalStatus === 403) {
+    console.log('Sending refresh token')
+
+    //Send refresh token to get new access token
+    const refreshResult = await baseQuery('/refresh', api, extraOptions) // '/refresh' is api endpoint in the backend to get refreshToken
+    console.log(refreshResult)
+
+    if (refreshResult?.data) {
+      const user = api.getState().auth.user
+
+      //store the new token
+      api.dispatch(setCredentials({ ...refreshResult.data, user }))
+
+      //retry the original query with new access token
+      result = await baseQuery(args, api, extraOptions)
+    } else {
+      api.dispatch(logOut())
+    }
+  }
+  return result
+}
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
+  baseQuery: baseQueryWithReauth,
   tagTypes: ['Post'],
-  
   endpoints: builder => ({})
 })
